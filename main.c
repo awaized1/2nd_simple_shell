c6743c7a3ec08968dd3b10ea8db91fc0ddf5c391
awaized1 <aduolaayomide@gmail.com>
2023-07-25 19:42:23 -0700

    Task 0 updated

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 
 /**
  * add - Adds two integers
- * @num1 - First integer to be added
- * @num2 - second integer to be added
+ * @num1: First integer to be added
+ * @num2: Second integer to be added
  *
- * Return - The result of addition.
+ * Return: The result of addition.
  */
 
 int add(int num1, int num2)
